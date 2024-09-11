51ec38e9b2e01b764387ec5fee56449ccb8bba8f
yinaihua <y17763312367@q63.com>
2024-09-11 09:21:43 +0800

    绘制图像

--- first.py
+++ first.py
@@ -4,3 +4,15 @@ print("test")
 print("代理之后测试")
 print("只能用命令行吗？")
 print("再试试")
+import numpy as np
+import matplotlib.pylab as plt
+
+def func(x):
+    return np.array(x>0,dtype=np.int)
+
+x=np.arange(-5.0,5.0,0.1)
+y=func(x)
+
+plt.plot(x,y)
+plt.ylim(-0.1,1.1)
+plt.show()
--- test2.py
+++ test2.py
@@ -1,1 +1,12 @@
-print("hello world")
+'''用python绘制函数图像'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+def func1(x):
+    return 0.01*x**2+0.1*x
+
+x=np.arange(0.0,20.0,0.1)
+y=func1(x)
+
+plt.plot(x,y)
+plt.show()
